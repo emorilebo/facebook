3d7fc15fe78d6cca08f87114f1199b19ab3571c5
Godfrey Lebo <emorylebo@gmail,com>
2021-08-15 13:05:43 +0100

    Stories component implemented

--- components/Feed.js
+++ components/Feed.js
@@ -1,9 +1,16 @@
+import InputBox from "./InputBox";
+import Stories from "./Stories";
+
 function Feed() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+      <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
+        <Stories />
+        <InputBox />
+        {/* Posts */}
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
