ac8d197f010287c764cffe1518b768b151622ed6
Godfrey Lebo <emorylebo@gmail,com>
2021-08-15 10:44:00 +0100

    Login implemented with nextauth...pretty tricky but smooth

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
-
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
-    <div >
+    <div>
       <Head>
         <title>Facebook</title>
         <meta name="description" content="Facebook by Godfrey Lebo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header />
       <main>
         {/* sidebar */}
         {/* feed */}
@@ -19,4 +21,14 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  //Get the user
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
 }
