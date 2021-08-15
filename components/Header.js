71035cf13b6d24ed5a45a4e8d1604c3dd6df6e62
Godfrey Lebo <emorylebo@gmail,com>
2021-08-15 09:07:09 +0100

    Header completed

--- components/Header.js
+++ components/Header.js
@@ -28,20 +28,34 @@ function Header() {
         />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
-          <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
+          <input
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
       {/* Center */}
       <div className="flex justify-center flex-grow">
-          <div className="flex space-x-6 md:space-x-2">
-              <HeaderIcon active Icon={HomeIcon}/>
-              <HeaderIcon Icon={FlagIcon}/>
-              <HeaderIcon Icon={PlayIcon}/>
-              <HeaderIcon Icon={ShoppingCartIcon}/>
-              <HeaderIcon Icon={UserGroupIcon}/>
-          </div>
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
       </div>
       {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* <Image
+
+          /> */}
+        <p className="whitespace-nowrap font-semibold pr-3">Godfrey Lebo</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 }
