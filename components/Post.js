0c86861e6b37a1bb946f7315c0ee89893696b08c
Godfrey Lebo <emorylebo@gmail,com>
2021-08-23 15:01:49 +0100

    fixing bug

--- components/Post.js
+++ components/Post.js
@@ -17,11 +17,11 @@ function Post({ name, message, email, postImage, image, timestamp }) {
           <div>
             <p className="font-medium">{name}</p>
             {timestamp ? (
-                <p className="text-xs text-gray-400">
-              {new Date(timestamp?.toDate()).toLocaleString()}
-            </p>
-            ): (
-                <p className="text-xs text-gray-400">Loading...</p>
+              <p className="text-xs text-gray-400">
+                {new Date(timestamp?.toDate()).toLocaleString()}
+              </p>
+            ) : (
+              <p className="text-xs text-gray-400">Loading...</p>
             )}
           </div>
         </div>
